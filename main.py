45178f518a6abd513464293e9384cebb0ff86f2e
JosephJeongs <jeongjoseph46@gmail.com>
2023-09-21 10:32:42 -0500

    added box layout and stacklayout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 
 
@@ -11,6 +13,13 @@ class MainWidget(Widget):
 
 class WidgetLayoutApp(App):
     pass
+
+
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "lr-tb"
+        b = Button(text="A", size_hint=(.5, .5))
 
 
 WidgetLayoutApp().run()
